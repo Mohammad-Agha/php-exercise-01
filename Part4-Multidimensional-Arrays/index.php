e1068d8940d0ddbbc0db9b973a35c7fb64ebd0a5
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-08 16:40:49 +0300

    Finished multidimensional array exercise

---
+++ Part4-Multidimensional-Arrays/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$multiArray = array(
+    "musicals" => array(
+        "Oklahoma", "The Music Man", "South Pacific"
+    ),
+    "dramas" => array(
+        "Lawrence of Arabia", "To Kill a Mockingbird", "Casablanca"
+    ),
+    "mysteries" => array(
+        "The Maltese Falcon", "Rear Window", "North by Northwest"
+    )
+);
+
+foreach($multiArray as $key => $array) {
+    echo strtoupper($key);
+    echo PHP_EOL;
+    foreach($array as $index => $item) {
+        echo "----> $index = $item";
+        echo PHP_EOL;
+    }
+}
